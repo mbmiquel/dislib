b2f0c6aebb302212227bb96b39bd18252b093f15
nihadness <1992.nihad@gmail.com>
2019-01-11 11:54:38 +0100

    Multiple files for C-SVM with DDS.

--- examples/test_dds.py
+++ examples/test_dds.py
@@ -7,12 +7,16 @@ from pycompss.dds import DDS
 
 from dislib.classification import CascadeSVM
 
+
 def main():
     file_path = sys.argv[1]
     print("Hola!")
 
     csvm=CascadeSVM(gamma=0.001, c=10000, max_iter=2000, check_convergence=True,
                     verbose=True)
+
+    def files_to_lines(file_pair):
+        return list(file_pair[1])
 
     def lines_to_dataset(lines, n_features, store_sparse=True):
         from tempfile import SpooledTemporaryFile
@@ -39,8 +43,11 @@ def main():
 
     # data = load_libsvm_file(file_path, subset_size=20, n_features=780)
 
-    data = DDS().load_text_file(
-        file_path, chunk_size=20, in_bytes=False, strip=False)\
+    # data = DDS().load_text_file(
+    #     file_path, chunk_size=20, in_bytes=False, strip=False)\
+    #     .map_partitions(lines_to_dataset, n_features=780)
+
+    data = DDS().load_files_from_dir(file_path).map_and_flatten(files_to_lines)\
         .map_partitions(lines_to_dataset, n_features=780)
 
     csvm.fit(data)
