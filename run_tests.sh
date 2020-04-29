41cf3446e3d40fefa005179cead52368bdffae6f
mbmiquel <mbmiquel@gmail.com>
2020-04-29 11:28:12 +0200

    fixed

--- run_tests.sh
+++ run_tests.sh
@@ -13,6 +13,7 @@ runcompss \
      --storage_conf="/dislib/storage_conf.cfg" \
      /dislib/tests/test_hecuba.py &> >(tee output.log)
 
+
 # Check the unittest output because PyCOMPSs exits with code 0 even if there
 # are failed tests (the execution itself is successful)
 result=$(cat output.log | egrep "OK|FAILED")
@@ -20,7 +21,7 @@ result=$(cat output.log | egrep "OK|FAILED")
 echo "Tests result: ${result}"
 
 # If word Failed is in the results, exit 1 so the pull request fails
-if [[ $result =~ FAILED ]]; then 
+if [[ $result =~ FAILED ]]; then
         exit 1
     fi
 
